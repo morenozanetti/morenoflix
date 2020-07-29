85d0a88033dc51dc9a71c4e6809386fb26c824e1
morenozanetti <morenozanetti@gmail.com>
2020-07-28 22:18:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Menu from './components/Menu'
+import DadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div style={{ background: "#141414"}}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={DadosIniciais.categorias[0].videos[0].titulo}
+        url={DadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é front-end? trabalhando na area"}
+      />
+      <Carousel
+      ignoreFirstVideo
+      category={DadosIniciais.categorias[0]}
+      />
+      <Carousel
+      category={DadosIniciais.categorias[1]}
+      />
+      <Carousel
+      category={DadosIniciais.categorias[2]}
+      />
+      <Footer
+      />
+    </div>
+  );
+}
+
+export default App;
